bc262e2a6a099ec5556c44d892693c437d47475d
KONO-anupam <anupam2work@gmail.com>
2025-05-10 11:28:15 +0000

    feat:added navbar and banner component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Cross, CrossIcon, Search, ShoppingCart, User, XIcon } from 'lucide-react';
+
+export default function Navbar() {
+  return (
+    <>
+      
+       <div className="w-full bg-black text-white text-center py-2 text-sm relative">
+      <p>
+        Sign up and get 20% off to your first order.{" "}
+        <span className="font-medium underline">Sign Up Now</span>
+      </p>
+      <button className="absolute right-4 top-2 pr-15">
+        <XIcon className="h-4 w-4 text-white" />
+      </button>
+    </div>
+      
+      <div className="w-full flex items-center justify-between px-16 py-4 bg-white">
+        
+        <div className="font-bold text-4xl  text-black">SHOP.CO</div>
+        
+        
+        <nav className="hidden md:flex items-center space-x-6">
+          <div className="relative group">
+            <a href="#" className="font-medium text-black">Shop</a>
+            <span className="ml-1 text-black">▼</span>
+          </div>
+          <a href="#" className="font-medium text-black">On Sale</a>
+          <a href="#" className="font-medium text-black">New Arrivals</a>
+          <a href="#" className="font-medium text-black">Brands</a>
+        </nav>
+        
+       <div className="hidden md:flex flex-1 max-w-md mx-6">
+  <div className="relative w-full">
+    <Search className="absolute left-3 top-2 text-gray-400 h-5 w-5" />
+    <input
+      type="text"
+      placeholder="Search for products..."
+      className="w-full pl-10 pr-4 py-2 bg-gray-100 rounded-full text-gray-800 text-sm"
+    />
+  </div>
+</div>
+
+        
+        <div className="flex items-center space-x-4 text-black">
+          <button>
+            <ShoppingCart className="h-6 w-6" />
+          </button>
+          <button>
+            <User className="h-6 w-6" />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
